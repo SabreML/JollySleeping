fab5ce99b78b34ae0530e365f2920bc2763162b3
SabreML <57483089+SabreML@users.noreply.github.com>
2023-01-26 15:01:34 +0000

    Initial positioning system
    Also a whole lot of reorganisation.

--- JollySleepingMod.cs
+++ JollySleepingMod.cs
@@ -3,169 +3,218 @@ using Menu;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
-[BepInPlugin("sabreml.jollysleeping", "JollySleeping", "0.1.0")]
-public class JollySleepingMod : BaseUnityPlugin
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace JollySleeping // Todo: Actual descriptive comments
 {
-	// List of the slugcat types of each player.
-	private List<string> playerSlugcatTypes;
-
-	public void OnEnable()
+	[BepInPlugin("sabreml.jollysleeping", "JollySleeping", "0.1.0")]
+	public class JollySleepingMod : BaseUnityPlugin
 	{
-		On.RainWorld.OnModsInit += OnInit;
-		On.StoryGameSession.CreateJollySlugStats += CreateJollySlugStatsHK;
-		On.Menu.MenuScene.ctor += MenuSceneHK;
-		On.Menu.MenuScene.SaveToFile += MenuScene_SaveToFile; // temp for testing
-		On.Menu.MenuScene.Update += MenuScene_Update; // temp for testing
-	}
+		// List of the slugcat types of each player.
+		private List<string> playerSlugcatTypes;
 
-	private void OnInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
-	{
-		orig(self);
+		// Dictionary of slugcat illustration positions
+		private Dictionary<string, Vector2> illustrationPositions;
 
-		// Verify that all of the new PNG files are properly formatted. (Alphabetical name order with dashes)
-		string[] fileList = AssetManager.ListDirectory($"Scenes{Path.DirectorySeparatorChar}Sleep Screen - JollySleeping");
-		foreach (string filePath in fileList)
+
+		/* ########## -SETUP- ########## */
+
+		public void OnEnable()
 		{
-			// Get just the file name without any slashes or the file extension.
-			string fileName = Regex.Match(filePath, string.Format("\\{0}(?!.*\\{0})(.*-.*).png", Path.DirectorySeparatorChar)).Groups[1].Value;
-			if (fileName == "")
+			On.RainWorld.OnModsInit += OnInit;
+			On.StoryGameSession.CreateJollySlugStats += CreateJollySlugStatsHK;
+			On.Menu.MenuScene.ctor += MenuSceneHK;
+
+			// Temporary for testing/debugging.
+			TestingTools.InitHooks();
+		}
+
+		private void OnInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+		{
+			orig(self);
+			illustrationPositions = ReadPositionsFile();
+			VerifyFileNames();
+		}
+
+		// positions file but reversed (better comment later)
+		private Dictionary<string, Vector2> ReadPositionsFile()
+		{
+			Dictionary<string, Vector2> output = new Dictionary<string, Vector2>();
+			string[] fileLines = File.ReadAllLines(AssetManager.ResolveFilePath($"Scenes{Path.DirectorySeparatorChar}Sleep Screen - JollySleeping{Path.DirectorySeparatorChar}positions.txt"));
+
+			// Parse the data
+			foreach (string line in fileLines)
 			{
-				continue; // Either not a PNG, or not properly formatted.
+				GroupCollection lineData = Regex.Match(line, @"(-*\d+), (-*\d+): (.*)").Groups;
+
+				Vector2 coordinates = new Vector2(float.Parse(lineData[1].Value), float.Parse(lineData[2].Value));
+				List<string> illustrations = Regex.Split(lineData[3].Value, ", ").ToList();
+				foreach (string illustration in illustrations)
+				{
+					output.Add(illustration, coordinates);
+				}
 			}
-			string[] fileNameParts = fileName.Split('-');
-			
-			// Check for alphabetical-ness.
-			bool alphabetical = true;
-			for (int i = 0; i < fileNameParts.Count() - 1; i++)
+			return output;
+		}
+
+		private void VerifyFileNames()
+		{
+			// Verify that all of the new PNG files are properly formatted. (Alphabetical name order with dashes)
+			string[] fileList = AssetManager.ListDirectory($"Scenes{Path.DirectorySeparatorChar}Sleep Screen - JollySleeping");
+			foreach (string filePath in fileList)
 			{
-				if (System.StringComparer.Ordinal.Compare(fileNameParts[i], fileNameParts[i + 1]) > 0)
-				{ // If the first string comes after the second alphabetically.
-					alphabetical = false;
+				// Get just the file name without any slashes or the file extension.
+				string fileName = Regex.Match(filePath, string.Format(@"\{0}(?!.*\{0})(.*-.*).png", Path.DirectorySeparatorChar)).Groups[1].Value;
+				if (fileName == "")
+				{
+					continue; // Either not a PNG, or not properly formatted.
+				}
+				string[] fileNameParts = fileName.Split('-');
+
+				// Check for alphabetical-ness.
+				bool alphabetical = true;
+				for (int i = 0; i < fileNameParts.Count() - 1; i++)
+				{
+					if (System.StringComparer.Ordinal.Compare(fileNameParts[i], fileNameParts[i + 1]) > 0)
+					{ // If the first string comes after the second alphabetically.
+						alphabetical = false;
+						break;
+					}
+				}
+				if (!alphabetical)
+				{
+					Debug.Log($"(JollySleeping) File {fileName}.png is not named alphabetically! This may cause issues later.");
 					break;
 				}
 			}
-			if (!alphabetical)
+		}
+
+
+		/* ########## -HOOKS- ########## */
+
+		private void CreateJollySlugStatsHK(On.StoryGameSession.orig_CreateJollySlugStats orig, StoryGameSession self, bool m)
+		{
+			orig(self, m);
+
+			// Make a copy of the slugcats being controlled by players.
+			playerSlugcatTypes = self.characterStatsJollyplayer
+				.Where(entry => entry != null) // Filter out null entries.
+				.Select(playerStats => playerStats.name.value.ToLower()) // Get the name of the slugcat they're playing as.
+				.Distinct() // Duplicate entires get merged into one for simplicity. (["gourmand", "gourmand", "gourmand", "rivulet"] -> ["gourmand", "rivulet"])
+				.ToList();
+			playerSlugcatTypes.Sort(); // Sort the list alphabetically.
+			Debug.Log("(JollySleeping) Players cached.");
+		}
+
+		private void MenuSceneHK(On.Menu.MenuScene.orig_ctor orig, MenuScene self, Menu.Menu menu, MenuObject owner, MenuScene.SceneID sceneID)
+		{
+			orig(self, menu, owner, sceneID);
+			if (self.sceneID != MenuScene.SceneID.SleepScreen || self.flatMode)
 			{
-				Debug.Log($"(JollySleeping) File {fileName}.png is not named alphabetically! This may cause bugs later.");
-				break;
+				return;
+			}
+			if (playerSlugcatTypes == null)
+			{
+				Debug.Log("(JollySleeping) Players not cached!");
+				return;
+			}
+
+			bool spearmaster = (ModManager.MSC && playerSlugcatTypes.Contains("spear"));
+			bool multiplayer = playerSlugcatTypes.Count > 1;
+
+			// Remove the existing foreground grass (1) and slugcat illustration (2).
+			string pattern = @"[12]";
+			if (spearmaster) // Spearmaster also has custom background grass (3) and their little drawings (d).
+			{
+				pattern = multiplayer ? @"[12d]" : @"[123d]"; // Don't need to remove the background grass for the multiplayer illustrations.
+			}
+			self.depthIllustrations.RemoveAll(item => Regex.IsMatch(item.fileName, pattern));
+			self.subObjects.RemoveAll(item => Regex.IsMatch((item as MenuDepthIllustration).fileName, pattern));
+
+			if (multiplayer)
+			{
+				// Use the custom art for different combinations of slugcats.
+				AddMultiplayerSlugcats(self, spearmaster);
+			}
+			else
+			{
+				// Use the default art for the slugcat they're playing as.
+				AddSingleplayerSlugcat(self, spearmaster);
 			}
 		}
-	}
 
-	private void CreateJollySlugStatsHK(On.StoryGameSession.orig_CreateJollySlugStats orig, StoryGameSession self, bool m)
-	{
-		orig(self, m);
-
-		// Make a copy of the slugcats being controlled by players.
-		playerSlugcatTypes = (from playerStats in self.characterStatsJollyplayer where playerStats != null select playerStats.name.value).ToList();
-		playerSlugcatTypes.Sort(); // Sort the list alphabetically.
-		Debug.Log("(JollySleeping) Player stats cached.");
-	}
-
-	private void MenuSceneHK(On.Menu.MenuScene.orig_ctor orig, MenuScene self, Menu.Menu menu, MenuObject owner, MenuScene.SceneID sceneID)
-	{
-		orig(self, menu, owner, sceneID);
-		if (self.sceneID != MenuScene.SceneID.SleepScreen || self.flatMode)
+		private void AddMultiplayerSlugcats(MenuScene self, bool spearmaster)
 		{
-			return;
-		}
-		if (playerSlugcatTypes == null)
-		{
-			Debug.Log("(JollySleeping) Player stats not cached!");
-			return;
-		}
-		if (playerSlugcatTypes.Count < 2) // Don't mess with anything if it's just singleplayer.
-		{
-			Debug.Log("(JollySleeping) < 2 players detected, using default illustration.");
-			return;
-		}
+			string folderName = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - JollySleeping";
+			string fileName = string.Join("-", playerSlugcatTypes);
+			Debug.Log($"(JollySleeping) Displaying {fileName}");
 
-		// Remove the existing slugcat illustration (2), foreground grass (1), and the Spearmaster's doodles if present (d).
-		string pattern = @"[21d]";
-		self.depthIllustrations.RemoveAll(item => Regex.IsMatch(item.fileName, pattern));
-		self.subObjects.RemoveAll(item => Regex.IsMatch((item as MenuDepthIllustration).fileName, pattern));
+			// Spearmaster doodles
+			//if (spearmaster)
+			//{
+			//	AddSpearmasterDoodle(self);
+			//}
 
-		AddSlugcatIllustrations(self);
-	}
+			// Add the slugcats to the scene.
+			MenuDepthIllustration slugcatIllustration = new MenuDepthIllustration(self.menu, self, folderName, fileName, new Vector2(568f, 33f), 1.7f,
+				MenuDepthIllustration.MenuShader.Normal);
+			self.AddIllustration(slugcatIllustration);
 
-	private void AddSlugcatIllustrations(MenuScene self)
-	{
-		string folderName = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - jollysleeping";
-		string fileName = string.Join("-", playerSlugcatTypes);
-		Debug.Log($"(JollySleeping) displaying {fileName}");
+			// Check if this illustration has a position override set.
+			if (illustrationPositions.ContainsKey(slugcatIllustration.fileName))
+			{
+				Debug.Log("Old: " + slugcatIllustration.pos);
+				slugcatIllustration.pos = illustrationPositions[slugcatIllustration.fileName];
+				Debug.Log("New: " + slugcatIllustration.pos);
+			}
 
-		// Spearmaster doodles
-		if (ModManager.MSC && playerSlugcatTypes.Contains("Spear"))
-		{
-			List<string> doodlePaths = GetRandomDoodle();
-
-			self.AddIllustration(new MenuDepthIllustration(self.menu, self, doodlePaths[0], doodlePaths[1], new Vector2(), 2.2f, MenuDepthIllustration.MenuShader.Basic)); // Todo: Positioning
+			// Add the foreground grass back in too. (Grass has to be added afterwards for layering)
+			string grassFolder = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - White";
+			self.AddIllustration(new MenuDepthIllustration(self.menu, self, grassFolder, "Sleep - 1", new Vector2(486f, -54f), 1.2f,
+				MenuDepthIllustration.MenuShader.Normal));
 		}
 
-		// Add the slugcats to the scene.
-		self.AddIllustration(new MenuDepthIllustration(self.menu, self, folderName, fileName, new Vector2(782f, 117f), 1.7f, MenuDepthIllustration.MenuShader.Normal)); // Todo: Positioning
-
-		// Add the foreground grass back in too. (Grass has to be added afterwards for layering)
-		string grassFolder = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - White";
-		self.AddIllustration(new MenuDepthIllustration(self.menu, self, grassFolder, "Sleep - 1", new Vector2(486f, -54f), 1.2f, MenuDepthIllustration.MenuShader.Normal));
-	}
-
-	// Returns the folder and file path for a random spearmaster doodle.
-	private List<string> GetRandomDoodle()
-	{
-		string folderPath = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - Spear";
-		string filePath = $"Sleep - D{Random.Range(1, 11)}";
-		return new List<string> { folderPath, filePath };
-	}
-
-
-
-	// ########## --DEBUGGING/TESTING METHODS BELOW-- ##########
-
-	private void MenuScene_SaveToFile(On.Menu.MenuScene.orig_SaveToFile orig, MenuScene self)
-	{
-		Debug.Log($"Positions : {self.sceneID.value}");
-		for (int i = 0; i < self.depthIllustrations.Count; i++)
+		private void AddSingleplayerSlugcat(MenuScene self, bool spearmaster)
 		{
-			MenuDepthIllustration target = self.depthIllustrations[i];
-			Debug.Log($"{target.fileName}   {target.pos}");
-		}
-	}
+			string playerSlugcat = playerSlugcatTypes[0];
+			self.sceneFolder = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - {playerSlugcat}";
+			Debug.Log($"(JollySleeping) Displaying Sleep - 2 - {playerSlugcat}");
 
-	// Used to loop through every illustration in the folder. Useful for quickly testing positioning.
-	private void MenuScene_Update(On.Menu.MenuScene.orig_Update orig, MenuScene self)
-	{
-		orig(self);
-		if (Input.GetKeyDown("l"))
+			// Spearmaster doodles
+			if (spearmaster)
+			{
+				// Unload and replace the cached 'Sleep - 3' atlas.
+				Futile.atlasManager.UnloadAtlas("Sleep - 3");
+				self.AddIllustration(new MenuDepthIllustration(self.menu, self, self.sceneFolder, "Sleep - 3", new Vector2(697f, 78f), 2.2f,
+					MenuDepthIllustration.MenuShader.Normal));
+
+				AddSpearmasterDoodle(self);
+
+				// Also unload the foreground grass atlas so that spearmaster's version can replace it below.
+				Futile.atlasManager.UnloadAtlas("Sleep - 1");
+			}
+
+			// Add the slugcat to the scene.
+			self.AddIllustration(new MenuDepthIllustration(self.menu, self, self.sceneFolder, $"Sleep - 2 - {playerSlugcat}", new Vector2(), 1.7f,
+				MenuDepthIllustration.MenuShader.Normal));
+			// Add the foreground grass back in too. (Grass has to be added afterwards for layering)
+			self.AddIllustration(new MenuDepthIllustration(self.menu, self, self.sceneFolder, "Sleep - 1", new Vector2(), 1.2f,
+				MenuDepthIllustration.MenuShader.Normal));
+
+			// Fix any positioning issues.
+			self.RefreshPositions();
+		}
+
+		// Picks and adds a random spearmaster doodle.
+		private void AddSpearmasterDoodle(MenuScene self)
 		{
-			GetNewIllustration(self);
+			string folderPath = $"Scenes{Path.DirectorySeparatorChar}Sleep Screen - Spear";
+			string filePath = $"Sleep - D{Random.Range(1, 11)}";
+			self.AddIllustration(new MenuDepthIllustration(self.menu, self, folderPath, filePath, new Vector2(), 2.2f,
+				MenuDepthIllustration.MenuShader.Basic)); // Todo: Multiplayer positioning
 		}
-	}
-	private void GetNewIllustration(MenuScene self)
-	{
-		MenuDepthIllustration currentIllust = self.depthIllustrations.Find(item => Regex.IsMatch(item.fileName, @"sleep - 2|(artificer|saint|gourmand|rivulet|spear|white|red|yellow)", RegexOptions.IgnoreCase));
-		MenuDepthIllustration grassIllust = self.depthIllustrations.Find(item => item.fileName.Contains("- 1")); // grass stuff
-
-		List<string> dirList = AssetManager.ListDirectory($"Scenes{Path.DirectorySeparatorChar}Sleep Screen - jollysleeping").ToList();
-		dirList.RemoveAll(item => item.Contains("positions.txt"));
-		int currentFileIndex = dirList.FindIndex(item => item.Contains(currentIllust.fileName.ToLower() + ".png"));
-
-		string targetFile = Regex.Match(dirList[(currentFileIndex + 1) % dirList.Count], @"\\(?!.*\\)(.*-.*).png").Groups[1].Value;
-
-		Debug.Log($"{currentIllust.fileName} -> {targetFile}");
-		self.AddIllustration(new MenuDepthIllustration(self.menu, self, currentIllust.folderName, targetFile, currentIllust.pos, currentIllust.depth, currentIllust.shader));
-		self.AddIllustration(new MenuDepthIllustration(self.menu, self, grassIllust.folderName, grassIllust.fileName, grassIllust.pos, grassIllust.depth, grassIllust.shader)); //grass
-
-		currentIllust.RemoveSprites();
-		self.depthIllustrations.Remove(currentIllust);
-		self.RemoveSubObject(currentIllust);
-		currentIllust = null; // probably don't need to null this but eh
-		grassIllust.RemoveSprites();
-		self.depthIllustrations.Remove(grassIllust);
-		self.RemoveSubObject(grassIllust);
-		grassIllust = null;
 	}
 }
---
+++ TestingTools.cs
@@ -0,0 +1,117 @@
+﻿using Menu;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace JollySleeping
+{
+	internal class TestingTools
+	{
+		public static void InitHooks()
+		{
+			On.Menu.MenuScene.SaveToFile += MenuScene_SaveToFile; // temp for testing
+			On.Menu.MenuScene.Update += MenuScene_Update; // temp for testing
+			On.Menu.MenuScene.RefreshPositions += MenuScene_RefreshPositions1;
+		}
+
+		private static void MenuScene_SaveToFile(On.Menu.MenuScene.orig_SaveToFile orig, MenuScene self)
+		{
+			Debug.Log($"Positions : {self.sceneID.value}");
+			for (int i = 0; i < self.depthIllustrations.Count; i++)
+			{
+				MenuDepthIllustration target = self.depthIllustrations[i];
+				Debug.Log($"{target.fileName}   {target.pos}");
+			}
+		}
+
+		private static void MenuScene_Update(On.Menu.MenuScene.orig_Update orig, MenuScene self)
+		{
+			orig(self);
+			if (Input.GetKeyDown("l"))
+			{
+				GetNewIllustration(self);
+			}
+			if (Input.GetKeyDown("m"))
+			{
+				ToggleMonk(self);
+			}
+		}
+		private static void GetNewIllustration(MenuScene self)
+		{
+			MenuDepthIllustration currentIllust = self.depthIllustrations.Find(item => Regex.IsMatch(item.fileName, @"sleep - 2|(artificer|saint|gourmand|rivulet|spear|white|red|yellow)", RegexOptions.IgnoreCase));
+			MenuDepthIllustration grassIllust = self.depthIllustrations.Find(item => item.fileName.Contains("- 1")); // grass stuff
+
+			List<string> dirList = AssetManager.ListDirectory($"Scenes/Sleep Screen - JollySleeping").ToList();
+			dirList.RemoveAll(item => item.Contains("positions.txt"));
+			int currentFileIndex = dirList.FindIndex(item => item.Contains(currentIllust.fileName.ToLower() + ".png"));
+
+			string targetFile = Regex.Match(dirList[(currentFileIndex + 1) % dirList.Count], @"\\(?!.*\\)(.*-.*).png").Groups[1].Value;
+
+			// add new stuff
+			Debug.Log($"{currentIllust.fileName} -> {targetFile}");
+			self.AddIllustration(new MenuDepthIllustration(self.menu, self, currentIllust.folderName, targetFile, currentIllust.pos, currentIllust.depth, currentIllust.shader));
+			self.AddIllustration(new MenuDepthIllustration(self.menu, self, grassIllust.folderName, grassIllust.fileName, grassIllust.pos, grassIllust.depth, grassIllust.shader)); //grass
+
+			// remove old stuff
+			currentIllust.RemoveSprites();
+			self.depthIllustrations.Remove(currentIllust);
+			self.RemoveSubObject(currentIllust);
+			currentIllust = null; // probably don't need to null this but eh
+			grassIllust.RemoveSprites();
+			self.depthIllustrations.Remove(grassIllust);
+			self.RemoveSubObject(grassIllust);
+			grassIllust = null;
+		}
+
+		private static MenuDepthIllustration tempMonk;
+
+		private static void ToggleMonk(MenuScene self)
+		{
+			if (tempMonk != null)
+			{
+				tempMonk.RemoveSprites();
+				self.depthIllustrations.Remove(tempMonk);
+				self.RemoveSubObject(tempMonk);
+				tempMonk = null;
+				Debug.Log("!konM");
+			}
+			else
+			{
+				tempMonk = new MenuDepthIllustration(self.menu, self, "Scenes/Sleep Screen - yellow", "sleep - 2 - yellow", new Vector2(782f, 117f), 1.7f, MenuDepthIllustration.MenuShader.Normal);
+				self.AddIllustration(tempMonk);
+				Debug.Log("Monk!");
+			}
+		}
+
+		private static void MenuScene_RefreshPositions1(On.Menu.MenuScene.orig_RefreshPositions orig, MenuScene self)
+		{
+			if (self.sceneID != MenuScene.SceneID.SleepScreen)
+			{
+				return;
+			}
+			Dictionary<string, string> before = LogIllustrationPos(self);
+			orig(self);
+			Dictionary<string, string> after = LogIllustrationPos(self);
+
+			foreach (KeyValuePair<string, string> pair in before)
+			{
+				if (pair.Value != after[pair.Key])
+				{
+					Debug.Log($"{pair.Key} pos changed from {pair.Value} --> {after[pair.Key]}");
+				}
+			}
+		}
+
+		private static Dictionary<string, string> LogIllustrationPos(MenuScene self)
+		{
+			// illustration.fileName: illustration.pos
+			Dictionary<string, string> result = new Dictionary<string, string>();
+			foreach (MenuDepthIllustration illustration in self.depthIllustrations)
+			{
+				result.Add(illustration.fileName, illustration.pos.ToString());
+			}
+			return result;
+		}
+	}
+}
